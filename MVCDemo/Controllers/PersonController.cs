e02281e07c4ed9accc22f40a851209a1e91ba1b1
Olivia <69110329+livrbecca@users.noreply.github.com>
2021-11-02 14:18:43 +0000

    Add project files.

---
+++ MVCDemo/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PersonController : Controller // controller
+    {
+        // GET: Person
+        public ActionResult Index() // action
+        {
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            people.Add(new PersonModel { FirstName = "Liv", LastName = "Sheeran", Age = 23}); // IsAlive defeaults to true, don't need to specify
+            people.Add(new PersonModel { FirstName = "Jane", LastName = "Alane", Age = 18 });
+            people.Add(new PersonModel { FirstName = "Phil", LastName = "Mitchell", Age = 63 });
+            people.Add(new PersonModel { FirstName = "M", LastName = "J", Age = 33, IsAlive=false});
+
+            return View(people);
+        }
+    }
+}
---
+++ MVCDemo/Models/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class PersonModel // model with 4 properties in it, could set default properties
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; } = 0;
+
+        public bool IsAlive { get; set; } = true;
+    }
+}
